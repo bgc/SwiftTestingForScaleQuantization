0634fcffabfa8222c3576d277a715c2243898e57
bgc <ilikesushi.org@gmail.com>
2016-02-23 22:20:22 +0000

    preparing xcode

--- Sources/MainGlobals.swift
+++ Sources/MainGlobals.swift
@@ -2,3 +2,18 @@ public struct MyGlobals {
 	static var rootNote: Int = 0;
 	static var scale: Int = 0;
 }
+
+public var noteNames: [String] = [
+	"C",
+	"C#",
+	"D",
+	"D#",
+	"E",
+	"F",
+	"F#",
+	"G",
+	"G#",
+	"A",
+	"A#",
+	"B"
+];
