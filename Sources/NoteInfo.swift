c12e58d76e75105928ab0baf8dbe74352f1e3056
gustavo costa <gustavo.costa@pacsis.pt>
2016-02-25 18:02:37 +0000

    init on NoteInfo struct

--- Sources/NoteInfo.swift
+++ Sources/NoteInfo.swift
@@ -5,4 +5,12 @@ public struct noteInfo {
 	var octave: Int
 	var noteName: String
 
+	init( midiNoteNumber: Int, noteNumber: Int, octave: Int, noteName: String)
+    {
+        self.midiNoteNumber = midiNoteNumber
+        self.noteNumber = noteNumber
+        self.octave = octave
+        self.noteName = noteName
+    }
+
 }
